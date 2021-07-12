4bbf5a476379416083a045c1b859207e86b62149
Andrey1de <andrey1de@gmail.com>
2021-07-12 06:54:57 +0300

    ClientApp

---
+++ wwwroot/precache-manifest.c537b7f89f4522fad6db852956132e90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9f83f3380a80ab478f19445c199241a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ecb07ab80ceeef9595d",
+    "url": "/static/css/2.a5f06b7f.chunk.css"
+  },
+  {
+    "revision": "f589e1f359325fd10654",
+    "url": "/static/css/main.4921690e.chunk.css"
+  },
+  {
+    "revision": "7ecb07ab80ceeef9595d",
+    "url": "/static/js/2.75d4080e.chunk.js"
+  },
+  {
+    "revision": "cacf9405a57a8029e6c84d91dcd46528",
+    "url": "/static/js/2.75d4080e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f589e1f359325fd10654",
+    "url": "/static/js/main.1abae6fb.chunk.js"
+  },
+  {
+    "revision": "394e8df64ae580c56791",
+    "url": "/static/js/runtime-main.1adc820a.js"
+  }
+]);
